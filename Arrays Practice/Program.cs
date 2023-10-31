70830e4dd7242aabe67ba02fab82721990a8b128
AugmentVista <AuAgments@gmail.com>
2023-10-31 14:30:28 -0300

    Initial commit

---
+++ Arrays Practice/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Arrays_Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Arrays");
+            Console.WriteLine("-----");
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.ReadKey(true);
+
+
+        }
+    }
+}
